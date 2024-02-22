404e3fb60fad805dc0d849b2a80077378fe9d40e
Luca Zoppetti <lucazoppetti@outlook.com>
2024-02-22 20:32:11 +0100

    complete 006

--- 006_orbits.py
+++ 006_orbits.py
@@ -1,13 +1,15 @@
 # This example was discussed during the lecture on the 12th of December, 2023
 
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 import numpy as np
 from scipy.integrate import solve_ivp
+from scipy.optimize import root_scalar
 from utils.polar import *
 
 M: float = 1
 r0: float = 1
-drdt0: float = 1
+E: float = -3.4
 phi0: float = 0
 
 
@@ -19,7 +21,10 @@ def V(r: float):
     return U(r) + M**2 / (2 * r**2)
 
 
-E = V(r0) + drdt0**2 / 2
+drdt0 = np.sqrt(2 * (E - V(r0)))
+
+r_min = root_scalar(lambda r: V(r) - E, bracket=[0.1, 5])
+r_max = root_scalar(lambda r: V(r) - E, bracket=[5, 10])
 
 
 # y is [r, dr/dt,, phi]
@@ -29,35 +34,67 @@ def system(_, y):
 
 
 t0 = 0
-tf = 180
+tf = 150
 result = solve_ivp(
-    system, [t0, tf], [r0, drdt0, phi0], t_eval=np.linspace(t0, tf, 1000)
+    system, [t0, tf], [r0, drdt0, phi0], t_eval=np.linspace(t0, tf, 10000)
 )
 
 
 fig = plt.figure(figsize=(10, 5))
 
-# Normal plot
+# Potential plot
 ax1 = fig.add_subplot(1, 2, 1)
 ax1.axhline(y=E, color="r", linestyle="--", label="E")
+ax1.set_title("Effective potential")
 ax1.set_xlabel("r")
 ax1.set_ylabel("V(r)")
 r_begin = 0.3
 r_end = 7.5
 ax1.plot(
-    np.linspace(r_begin, r_end, 100), [V(r) for r in np.linspace(r_begin, r_end, 100)]
+    np.linspace(r_begin, r_end, 100),
+    [V(r) for r in np.linspace(r_begin, r_end, 100)],
+    label="V(r)",
 )
 ax1.legend()
 
-# Polar plot
-ax2 = fig.add_subplot(1, 2, 2)
-ax2.set_aspect("equal")
-ax2.grid(True)
-time_step = (tf - t0) / 1000
+# Orbit plot
 r_values = result.y[0]
 phi_values = result.y[2]
 x_values, y_values = to_cartesian(r_values, phi_values)
-ax2.plot(x_values, y_values, label="Orbit", color="tab:orange")
+
+ax2 = fig.add_subplot(1, 2, 2)
+ax2.set_aspect("equal")
+ax2.set_title("Orbit")
+ax2.grid(True)
+
+ax2.plot(x_values[0], y_values[0], marker="o", color="r")
+ax2.plot(x_values[-1], y_values[-1], marker="o", color="r")
+
+# Plot orbit
+ax2.plot(x_values, y_values, label="Orbit", color="r")
+
+# Plot limits: r_min and r_max
+ax2.add_patch(
+    patches.Circle(
+        (0, 0),
+        r_min.root,
+        fill=False,
+        color="tab:green",
+        zorder=2,
+        label="Minimum radius",
+    )
+)
+ax2.add_artist(
+    patches.Circle(
+        (0, 0),
+        r_max.root,
+        fill=False,
+        color="tab:purple",
+        zorder=2,
+        label="Maximum radius",
+    )
+)
+
 ax2.legend()
 
 plt.show()
--- utils/polar.py
+++ utils/polar.py
@@ -2,7 +2,7 @@ from typing import Tuple
 import numpy as np
 
 
-def to_cartesian(r: float, phi: float) -> Tuple[float, float]:
+def to_cartesian(r, phi):
     x = r * np.cos(phi)
     y = r * np.sin(phi)
     return (x, y)
